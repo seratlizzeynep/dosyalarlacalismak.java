d52bb04f692a15b9bca04616c86c8c4fc7e89c16
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-10-11 21:57:06 +0300

    Add files via upload

---
+++ readingFiles2.java
@@ -0,0 +1,76 @@
+package workingWithFiles;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class readingFiles2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		createFile();
+		getFileInfo();
+		readFile();
+	}
+
+	public static void createFile() {
+
+		File file = new File("C:\\Users\\Gokhan\\Desktop\\eclipse\\files\\ student.txt");
+		
+		try {
+
+		if (file.createNewFile()) {
+			
+			System.out.println("Dosya oluşturuldu.");
+		} else {
+
+			System.out.println("Dosya zaten var.");
+		}
+				
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public static void getFileInfo() {
+		
+		File file = new File("C:\\Users\\Gokhan\\Desktop\\eclipse\\files\\ student.txt");
+		
+		
+	if (file.exists()) {
+		
+		System.out.println("Dosya adi:" + file.getName());
+		System.out.println("Dosya yolu:" + file.getAbsolutePath());
+		System.out.println("Dosya yazilabilir mi:" + file.canWrite());
+		System.out.println("Dosya okunabilir mi:" + file.canRead());
+		System.out.println("Dosyanin byte cinsinden boyutu:" + file.length());}
+		
+		
+	} 
+	
+public static void readFile() {
+		
+		File file = new File("C:\\Users\\Gokhan\\Desktop\\eclipse\\files\\ student.txt");
+		
+
+		try {
+
+			Scanner reader = new Scanner(file);
+				while (reader.hasNextLine()) {
+					String line =  reader.nextLine();
+					System.out.println(line);
+				}
+			
+				reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+
